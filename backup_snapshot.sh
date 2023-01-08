23d7255aa1e1b30db8c91114f103742687bfc48e
p4we1 <104769030+p4we1@users.noreply.github.com>
2023-01-08 13:26:54 +0100

    Initial commit

--- backup_snapshot.sh
+++ backup_snapshot.sh
@@ -20,20 +20,22 @@ lf=/var/log/backup/backup.log
 synclog=/var/log/backup/backup_sync.log
 
 # srcdata - source folder to snapshot
-srcdata=/home/master/testssl.sh
+srcdata=
 
 # destdata - destination folder to store snapshoted data
 # use remote storage mounted to local folder
 # descdata=/backup/smartsite/daily.00
-destdata=/opt/backup
+destdata=
 
 # count_backup - Days to store snapshoted data
 # set value of days to store snapshot
+# default setting 14 days - set as you wish
 count_backup=14
 
 # EXCLUDE files and folder from snapshot
 # *.bak
 # */temp/*
+# Variable not used - use file excluded-list.txt - one line one exclude
 exclude=''
 # Variables =========================================END
 
@@ -47,6 +49,7 @@ function dt {
     date +%Y-%m-%d-%H:%M:%S
 }
 
+# not used, add excluded files and dirs by exclude-list.txt
 function exclude {
  all_exclude=""
  for exclude_part in $exclude
@@ -59,24 +62,39 @@ echo $all_exclude
 }
 # Function ==========================================END
 
+# Check if variable are set in script. if not exit =====
+
+if [ -z "${srcdata}" ] || [ -z "${destdata}" ];
+then
+    echo "Please set both srcdata and destdata variables"
+    exit 0
+fi
+
+if [ ! -d "$srcdata" ];
+then
+ echo $"Directory srcdata not exist"
+ exit 0
+fi
+
+if [ ! -d "$destdata" ];
+then
+ echo $"Directory destdata not exist"
+ exit 0
+fi
+
+
+
+# Check  ============================================END
+
+
+
 # Start ================================================
 destdataD="${destdata}/daily.00"
 
 echo $"==============================~backup_snapshot.sh~===================" >> $lf
 echo $"= $name Snapshot Script" >> $lf
 echo $"==============================~backup_snapshot.sh~===================" >> $lf
-echo $"[FILE-SYNC]:`dt`: FILE SYNC" >>$lf
-srcdatadirsize=`du -hs $srcdata`
-echo $"[FILE-SYNC]:`dt`: Directory size: $srcdatadirsize" >>$lf
-echo $"[FILE-SYNC]:`dt`: SYNC START" >> $lf
-# rsync -vah --delete --stats --log-file="$synclog" --exclude '*/temp/*' $srcdata $destdata >> $synclog
-#rsync -vah --delete --stats --log-file="$synclog" `exclude` $srcdata $destdataD >> $synclog
-rsync -vah --delete --stats --log-file="$synclog" --exclude-from={'exclude-list.txt'} $srcdata $destdataD >> $synclog
-#rsync -vah --delete --stats --log-file="$synclog" $srcdata $destdataD  >> $synclog
-#destdirsize=`du -hs $destdata/daily.00`
-destdirsize=`du -hs $destdataD`
-echo $"[FILE-SYNC]:`dt`: SYNC STOP" >> $lf
-echo $"[FILE-SYNC]:FOLDER SIZE: $destdirsize" >> $lf
+
 echo $"[FILE-ROTATE]:`dt`: ROTATE START" >> $lf
 if [ -d "$destdata/daily.`convert1.01 $((count_backup-1))`" ] ; then
  echo $"[FILE-SYNC]:`dt`: Delete oldest day" >> $lf
@@ -101,7 +119,23 @@ cp -al "$destdata/daily.00" "$destdata/daily.01"
 echo $"[FILE-ROTATE]:`dt`: daily.00 to daily.01 - copied" >> $lf
 fi;
 echo $"[FILE-ROTATE]:`dt`: All data moved" >> $lf
+
+
+echo $"[FILE-SYNC]:`dt`: FILE SYNC" >>$lf
+srcdatadirsize=`du -hs $srcdata`
+echo $"[FILE-SYNC]:`dt`: Directory size: $srcdatadirsize" >>$lf
+echo $"[FILE-SYNC]:`dt`: SYNC START" >> $lf
+# rsync -vah --delete --stats --log-file="$synclog" --exclude '*/temp/*' $srcdata $destdata >> $synclog
+#rsync -vah --delete --stats --log-file="$synclog" `exclude` $srcdata $destdataD >> $synclog
+rsync -vah --delete --stats --log-file="$synclog" --exclude-from='exclude-list.txt' $srcdata $destdataD >> $synclog
+#rsync -vah --delete --stats --log-file="$synclog" $srcdata $destdataD  >> $synclog
+#destdirsize=`du -hs $destdata/daily.00`
+destdirsize=`du -hs $destdataD`
+echo $"[FILE-SYNC]:`dt`: SYNC STOP" >> $lf
+echo $"[FILE-SYNC]:FOLDER SIZE: $destdirsize" >> $lf
+
+
 echo $"[FILE-BAK]:`dt`: $name SNAPSHOT SCRIPT STOP" >> $lf
 echo $"------------------------------~backup_snapshot.sh--------------------" >> $lf
 touch $destdata/daily.00
-# STOP MAKING STUPID PEOPLE FAMOUS =======================
+# STOP = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
